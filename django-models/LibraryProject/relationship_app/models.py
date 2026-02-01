b5ccc3d307f5cebf79b7be4100305d715e968103
Leoul Zerihun <leoulzerihun06@gmail.com>
2026-02-01 11:37:55 +0300

    updated commit

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,7 +1,4 @@
 from django.db import models
-from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 
 class Author(models.Model):
@@ -14,22 +11,18 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(
-        Author, on_delete=models.CASCADE, related_name="books")
+        Author,
+        on_delete=models.CASCADE,
+        related_name='books'
+    )
 
     def __str__(self):
         return self.title
 
-    class Meta:
-        permissions = [
-            ("can_add_book", "Can add book"),
-            ("can_change_book", "Can change book"),
-            ("can_delete_book", "Can delete book"),
-        ]
-
 
 class Library(models.Model):
-    name = models.CharField(max_length=100)
-    books = models.ManyToManyField(Book, related_name="libraries")
+    name = models.CharField(max_length=150)
+    books = models.ManyToManyField(Book, related_name='libraries')
 
     def __str__(self):
         return self.name
@@ -38,30 +31,10 @@ class Library(models.Model):
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(
-        Library, on_delete=models.CASCADE, related_name="librarian")
+        Library,
+        on_delete=models.CASCADE,
+        related_name='librarian'
+    )
 
     def __str__(self):
         return self.name
-
-
-# ---- ADD FOR TASK 3 (Role-Based Access Control) ----
-
-class UserProfile(models.Model):
-    ROLE_CHOICES = (
-        ('Admin', 'Admin'),
-        ('Librarian', 'Librarian'),
-        ('Member', 'Member'),
-    )
-
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(
-        max_length=20, choices=ROLE_CHOICES, default='Member')
-
-    def __str__(self):
-        return f"{self.user.username} - {self.role}"
-
-
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        UserProfile.objects.create(user=instance)

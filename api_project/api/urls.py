7124b3c8e77862cf2de032e81ff9b4821abbcb59
Leoul Zerihun <leoulzerihun06@gmail.com>
2026-02-01 11:23:45 +0300

    first commit

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,15 @@
-from django.urls import path
-from .views import BookList   # import the view
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookList, BookViewSet
+
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
+    # Route for the BookList view (ListAPIView)
     path('books/', BookList.as_view(), name='book-list'),
+
+    # Include router-generated URLs (CRUD)
+    path('', include(router.urls)),
 ]

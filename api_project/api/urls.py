06f8ae6c1440cedd4183bcf7f0344dba7700b1ab
Leoul Zerihun <leoulzerihun06@gmail.com>
2026-02-01 11:16:24 +0300

    first commit

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,14 +1,9 @@
-from django.urls import path
-from .views import BookList
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include   # include is needed
 
 urlpatterns = [
-    path('books/', BookList.as_view(), name='book-list'),
-]
+    path('admin/', admin.site.urls),
 
-
-urlpatterns = [
-    path('admin/', admin.site.urls),   # keep this
-    path('api/', include('api.urls')),  # add this
+    # ✅ Add this line to include your api app’s URLs
+    path('api/', include('api.urls')),
 ]

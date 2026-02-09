ba0cbeb91d6a03dd8f8e73c738d89c2a98fc1c01
Leoul Zerihun <leoulzerihun06@gmail.com>
2026-02-09 21:02:31 +0300

    first commit

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -29,3 +29,24 @@ class BookListView(generics.ListAPIView):
     # Ordering
     ordering_fields = ["title", "publication_year"]
     ordering = ["title"]  # default ordering
+
+
+# Create new book (restricted to authenticated users)
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+
+
+# Update existing book (restricted to authenticated users)
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+
+
+# Delete book (restricted to authenticated users)
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]

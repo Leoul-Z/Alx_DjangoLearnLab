43992957edaffd588cdc710a7db73946156ce56b
Leoul Zerihun <leoul.zerihun@aastustudent.edu.et>
2026-02-22 12:32:03 +0300

    Add files via upload

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -8,6 +8,12 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from .serializers import RegisterSerializer, LoginSerializer, UserSerializer
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.contrib.auth import get_user_model, authenticate
+from rest_framework.authtoken.models import Token
+from .serializers import RegisterSerializer, LoginSerializer, UserSerializer
 
 
 class FollowUserView(APIView):
@@ -28,8 +34,25 @@ class UnfollowUserView(APIView):
         return Response({"message": f"You unfollowed {target_user.username}"})
 
 
-class RegisterView(generics.CreateAPIView):
+CustomUser = get_user_model()
+
+# Registration
+
+
+class RegisterView(generics.GenericAPIView):
     serializer_class = RegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response({
+            "user": UserSerializer(user).data,
+            "token": token.key
+        }, status=status.HTTP_201_CREATED)
+
+# Login
 
 
 class LoginView(APIView):
@@ -38,10 +61,13 @@ class LoginView(APIView):
         serializer.is_valid(raise_exception=True)
         return Response(serializer.validated_data)
 
+# Profile
+
 
 class ProfileView(generics.RetrieveUpdateAPIView):
+    queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_object(self):
         return self.request.user

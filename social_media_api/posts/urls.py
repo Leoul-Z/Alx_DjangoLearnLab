662cf0f8b615e0017c8b169b8750fc47a3aa74b0
Leoul Zerihun <leoul.zerihun@aastustudent.edu.et>
2026-02-22 12:36:17 +0300

    Add files via upload

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,5 +1,5 @@
-from django.urls import path
 from rest_framework.routers import DefaultRouter
+from django.urls import path
 from .views import PostViewSet, CommentViewSet, FeedView
 
 router = DefaultRouter()

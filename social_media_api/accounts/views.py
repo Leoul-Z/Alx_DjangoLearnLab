7add75b738b46ee4514da811ea384e17bac72b74
Leoul Zerihun <leoul.zerihun@aastustudent.edu.et>
2026-02-22 12:48:32 +0300

    Add files via upload

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -15,6 +15,33 @@ from django.contrib.auth import get_user_model, authenticate
 from rest_framework.authtoken.models import Token
 from .serializers import RegisterSerializer, LoginSerializer, UserSerializer
 
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from .models import User
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def follow_user(request, user_id):
+    target_user = get_object_or_404(User, id=user_id)
+
+    if target_user == request.user:
+        return Response({"error": "You cannot follow yourself"}, status=400)
+
+    request.user.following.add(target_user)
+    return Response({"message": f"You are now following {target_user.username}"})
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def unfollow_user(request, user_id):
+    target_user = get_object_or_404(User, id=user_id)
+
+    request.user.following.remove(target_user)
+    return Response({"message": f"You unfollowed {target_user.username}"})
+
 
 class FollowUserView(APIView):
     permission_classes = [IsAuthenticated]

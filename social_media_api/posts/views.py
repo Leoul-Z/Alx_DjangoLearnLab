ca72451edbaf29c22a224d0ed4e32e2ecc0af4dd
Leoul Zerihun <leoul.zerihun@aastustudent.edu.et>
2026-02-22 12:34:11 +0300

    Add files via upload

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,6 +1,10 @@
 from rest_framework import viewsets, permissions, filters
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
+
+# Permission class to restrict edits/deletes to owners
 
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
@@ -8,6 +12,8 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.author == request.user
+
+# Post CRUD
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -21,6 +27,8 @@ class PostViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
+# Comment CRUD
+
 
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all().order_by('-created_at')
@@ -30,3 +38,18 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+# Feed view
+
+
+class FeedView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        # Get all users the current user follows
+        following_users = request.user.following.all()
+        # Get posts authored by those users, newest first
+        posts = Post.objects.filter(
+            author__in=following_users).order_by('-created_at')
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)

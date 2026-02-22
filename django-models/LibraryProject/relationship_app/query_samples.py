af80c39750b274a81f3fd86e56f95a8634a07ec4
Leoul Zerihun <leoul.zerihun@aastustudent.edu.et>
2026-02-22 14:32:04 +0300

    Add files via upload

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -25,11 +25,11 @@ def books_in_library(library_name):
 
 def librarian_of_library(library_name):
     library = Library.objects.get(name=library_name)
-    return library.librarian
+    # Explicit query using OneToOneField
+    return Librarian.objects.get(library=library)
 
 
 if __name__ == "__main__":
-    # Example usage
     print("Books by J.K. Rowling:", books_by_author("J.K. Rowling"))
     print("Books in Central Library:", books_in_library("Central Library"))
     print("Librarian of Central Library:",
